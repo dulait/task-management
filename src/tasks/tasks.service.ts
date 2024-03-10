0bea7a79d72c28614c942c7169ee564636d4a3fa
dulait <dd20211096@student.fon.bg.ac.rs>
2024-03-10 18:05:49 +0100

    add 'getTaskById()'method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,6 +12,12 @@ export class TasksService {
         return this.tasks;
     }
 
+    getTaskById(id: string): Task {
+        return this.tasks.find((task) => {
+            task.id === id;
+        })
+    }
+
     createTask(createTaskDto: CreateTaskDTO): Task {
         const { title, description } = createTaskDto;
 
