508199e62b38e5b800d3edfb4e394211b0919ac9
dulait <dd20211096@student.fon.bg.ac.rs>
2024-03-12 09:01:23 +0100

    add user info to task crud operations

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -23,8 +23,11 @@ export class TasksController {
     }
 
     @Get('/:id')
-    getTaskById(@Param('id') id: string): Promise<Task> {
-        return this.tasksService.getTaskById(id);
+    getTaskById(
+        @Param('id') id: string,
+        @GetUser() user: User,
+    ): Promise<Task> {
+        return this.tasksService.getTaskById(id, user);
     }
 
     @Post()
@@ -36,17 +39,21 @@ export class TasksController {
     }
 
     @Delete('/:id')
-    deleteTask(@Param('id') id: string): Promise<void> {
-        return this.tasksService.deleteTask(id);
+    deleteTask(
+        @Param('id') id: string,
+        @GetUser() user: User,
+    ): Promise<void> {
+        return this.tasksService.deleteTask(id, user);
     }
 
     @Patch('/:id/status')
     updateTaskStatus(
         @Param('id') id: string,
         @Body() updateTaskStatusDto: UpdateTaskStatusDTO,
+        @GetUser() user: User,
     ): Promise<Task> {
         const { status } = updateTaskStatusDto;
-        return this.tasksService.updateTaskStatus(id, status);
+        return this.tasksService.updateTaskStatus(id, status, user);
     }
 
 }
